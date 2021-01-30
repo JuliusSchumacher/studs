cf9411ab1054be47f370a09a2cc1407e4a2ad059
Julius Schumacher <juliusschumacher@gmail.com>
2021-01-30 12:18:02 +0100

    fetch target from command lin

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-//use std::env;
+use std::env;
+use std::str::FromStr;
 //use std::io::{Read,Write};
-//use std::net::IpAddr;
+use std::net::IpAddr;
 use fastping_rs::Pinger;
 use fastping_rs::PingResult::{Idle, Receive};
-//#[macro_use]
 use dns_lookup::lookup_host;
 use rasciigraph::{plot, Config};
 
+struct Target{
+    hostname: String,
+    ip: IpAddr
+}
+
 fn main() {
-    let hostname = "google.com";
-    let ips: Vec<std::net::IpAddr> = lookup_host(hostname).unwrap();
-    println!("{}", ips[0]);
+    let target = get_target();
+
+    //create pinger
     let (pinger, results) = match Pinger::new(None, None) {
         Ok((pinger, results)) => (pinger, results),
         Err(e) => panic!("Error creating Pinger: {}", e)
     };
 
-    pinger.add_ipaddr(&ips[0].to_string());
+    pinger.add_ipaddr(&target.ip.to_string());
     pinger.run_pinger();
 
     let mut pings = Vec::new();
@@ -28,9 +33,9 @@ fn main() {
                     Idle{addr} => {
                         println!("Idle Address {}.", addr);
                     },
-                    Receive{rtt, addr} => {
+                    Receive{rtt,..} => {
                         pings.push(rtt.as_millis() as f64);
-                        let p = plot(pings.clone(), Config::default().with_height(5).with_caption(hostname.to_string()));
+                        let p = plot(pings.clone(), Config::default().with_height(9).with_caption(target.hostname.to_string()));
                         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
                         println!("{}", p);
                     }
@@ -41,4 +46,21 @@ fn main() {
     }
 }
 
+//get target from command line arg
+fn get_target() -> Target {
+
+    let arg:Vec<String>= env::args().collect();
+
+    //first try to resolve as pure IP
+    let target:Target = match IpAddr::from_str(&arg[1]) {
+        Ok(ip) => Target {hostname : String::from(""), ip : ip},
+        //otherwise try to resolve as hostname
+        Err(_e) => match lookup_host(&arg[1]) {
+            Ok(ips) => Target {hostname : arg[1].clone() , ip : ips[0]},
+            Err(_e) => panic!("Could not resovle!")
+        }
+    };
+
+    return target;
+}
 
